5841a2f6057ae6db6c1c551123f0a74b3302f350
Arkanheal <27277903+Arkanheal@users.noreply.github.com>
2023-07-25 21:52:30 +0200

    Switched to public props
    Makes it easier to stringify

--- src/project.js
+++ src/project.js
@@ -1,41 +1,18 @@
 export default class Project {
-  #name;
-  #todos;
-  #selected
 
   constructor(name, selected) {
-    this.#name = name;
-    this.#todos = [];
-    this.#selected = selected;
-  }
-
-  set name(name) {
-    this.#name = name;
-  }
-
-  set selected(selected) {
-    this.#selected = selected;
-  }
-
-  get name() {
-    return this.#name;
-  }
-
-  get selected() {
-    return this.#selected;
-  }
-
-  get todos() {
-    return this.#todos;
+    this.name = name;
+    this.todos = [];
+    this.selected = selected;
   }
 
   addTodo(todo) {
-    this.#todos.push(todo);
+    this.todos.push(todo);
   }
 
   removeTodo(title) {
-    this.#todos = this.#todos.filter(t => t.title !== title);
+    this.todos = this.todos.filter(t => t.title !== title);
   }
-}
+};
 
 export let projects = {"All Projects": new Project("All Projects", true)};
--- src/todo.js
+++ src/todo.js
@@ -1,61 +1,9 @@
 export default class Todo {
-  #title;
-  #description;
-  #dueDate;
-  #priority;
-  #project;
-
   constructor(title, description, dueDate, priority, project) {
-    if (!priorities.includes(priority.toUpperCase())) {
-      throw new Error("Invalid priority value.");
-    }
-
-    this.#title = title;
-    this.#description = description;
-    this.#dueDate = dueDate;
-    this.#priority = priority;
-    this.#project = project;
-  }
-
-  get title() {
-    return this.#title;
-  }
-
-  get description() {
-    return this.#description;
-  }
-
-  get dueDate() {
-    return this.#dueDate;
-  }
-
-  get priority() {
-    return this.#priority;
-  }
-
-  get project() {
-    return this.#project;
-  }
-
-  set title(title) {
-    this.#title = title;
-  }
-
-  set description(description) {
-    this.#description = description;
-  }
-
-  set dueDate(dueDate) {
-    this.#dueDate = dueDate;
-  }
-
-  set priority(priority) {
-    if (!priorities.includes(priority.toUpperCase())) {
-      throw new Error("Invalid Priority value.");
-    }
-    this.#priority = priority;
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.project = project;
   }
 }
-
-const priorities = ["LOW", "MEDIUM", "HIGH"];
-
